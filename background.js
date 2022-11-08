c0b6576c8c59caf648dcb6cbda793c20f40d9c95
aleruiz <ales.email@gmail.com>
2022-11-08 09:54:04 -0500

    Add first pass at extension

---
+++ background.js
@@ -0,0 +1,28 @@
+// platform agnostic shenanigans
+if (typeof browser === "undefined") {
+    var browser = chrome;
+}
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {message: 'checks-for-everyone'}
+    ).then(response => {
+      console.log("Message from the content script:");
+      console.log(response.response);
+    }).catch(onError);
+  }
+}
+
+browser.browserAction.onClicked.addListener(function(){
+  console.log('Triggering extension');
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs).catch(onError);
+});
